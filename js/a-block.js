70b9f66df3e0b15fd07ce7834418733252eacc01
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-04 21:13:34 +0600

    Update a-block.js

--- js/a-block.js
+++ js/a-block.js
@@ -1,1 +1,12 @@
-A
+ymaps.ready(init);
+    function init() {
+      var geolocation = ymaps.geolocation,
+        coords = [geolocation.latitude, geolocation.longitude],
+        myMap = new ymaps.Map("get_info", { center: coords, zoom: 10 });
+      var country = geolocation.country;
+      var city = geolocation.city;
+      if (country != "Польша") {
+      window.location.href = "/block";
+     }else { window.location.href = "/main";
+   }
+}
