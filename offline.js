b3c8656905d73c94a443aefad6b39034d7cf54a9
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-06 00:15:37 +0600

    Delete offline.js

--- offline.js
+++
@@ -1,9 +0,0 @@
-	var date = new Date();
-        var hour = date.getHours();
-        if (hour>=5 && hour<12) online = "online"; else {
-        if (hour>=12 && hour<18) online = "online"; else {
-        if (hour>=18 && hour<24) online = "online"; else {
-        if (hour>=0 && hour<5) online = "offline"; }
-    }
-}
-if(online == "offline") { window.location.href = "/offline"; }
