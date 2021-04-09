e466d10bcb443e6e0cb40553b3a5fa0fca395ed2
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-09 22:35:38 +0600

    Update offline.js

--- js/offline.js
+++ js/offline.js
@@ -1,13 +1,1 @@
-var date = new Date();
-      var hour = date.getHours();
-      if (hour>=22 && hour<24) online = "offline"; else {
-      if (hour>=0 && hour<8) online = "offline"; else {
-      if (hour>=8 && hour<15) online = "offline"; else {
-      if (hour>=15 && hour<22) online = "offline"; }
-   }
-}
-if (online == "offline") {
 window.location.href = "/offline";
-} else if (online == "teh") {
-window.location.href = "/server";
-} else { }
