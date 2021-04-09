a0b5c525bc199e3135a3850148fcc5b8bd2b7be3
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-09 20:57:23 +0600

    Create online.js

---
+++ js/online.js
@@ -0,0 +1,13 @@
+var date = new Date();
+      var hour = date.getHours();
+      if (hour>=22 && hour<24) online = "online"; else {
+      if (hour>=0 && hour<8) online = "online"; else {
+      if (hour>=8 && hour<15) online = "online"; else {
+      if (hour>=15 && hour<22) online = "online"; }
+   }
+}
+if (online == "online") {
+window.location.href = "/index";
+} else if (online == "teh") {
+window.location.href = "/index";
+} else { }
