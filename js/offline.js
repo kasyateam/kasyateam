cad4c2286df4819963159de48b5e31782f9cd185
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-09 16:06:39 +0600

    Update offline.js

--- js/offline.js
+++ js/offline.js
@@ -1,13 +1,13 @@
 var date = new Date();
         var hour = date.getHours();
-        if (hour>=7 && hour<14) online = "1"; else {
-        if (hour>=14 && hour<15) online = "1"; else {
-        if (hour>=0 && hour<1) online = "1"; else {
-        if (hour>=1 && hour<7) online = "0"; }
+        if (hour>=7 && hour<14) online = "0"; else {
+        if (hour>=14 && hour<15) online = "0"; else {
+        if (hour>=0 && hour<1) online = "0"; else {
+        if (hour>=1 && hour<7) online = "1"; }
     }
 }
-if (online == "1") {
+if (online == "0") {
   window.location.href = "/offline";
-} else if (online == "0") {
+} else if (online == "1") {
   window.location.href = "/error";
 } else { }
