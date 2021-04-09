b882b5df58ca16d7e53d523161836e851a4093ae
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-09 15:37:41 +0600

    Update offline.js

--- js/offline.js
+++ js/offline.js
@@ -1,14 +1,12 @@
 var date = new Date();
         var hour = date.getHours();
-        if (hour>=7 && hour<14) online = "2"; else {
-        if (hour>=14 && hour<15) online = "2"; else {
-        if (hour>=0 && hour<1) online = "2"; else {
-        if (hour>=1 && hour<7) online = "2"; }
+        if (hour>=7 && hour<14) online = "0"; else {
+        if (hour>=14 && hour<15) online = "0"; else {
+        if (hour>=0 && hour<1) online = "0"; else {
+        if (hour>=1 && hour<7) online = "0"; }
     }
 }
 if(online == "0") { 
-alert("Отказан блок D"); } 
-if else (online == "1") { alert("Отказан блок R");}
-else { window.location.href = "/error";}
+window.location.href = "/error";}
 
 
