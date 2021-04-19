12221fbac2f0af90499f97a71de49498b0053bee
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-19 19:28:10 +0600

    Create VPN.js

---
+++ assets/kasyateam/js/VPN.js
@@ -0,0 +1,11 @@
+	ymaps.ready(init);
+	function init() {
+		var geolocation = ymaps.geolocation, 
+		coords = [geolocation.latitude, geolocation.longitude], 
+		myMap = new ymaps.Map("get_info", {center: coords, zoom: 10});
+		var country = geolocation.country;
+		var city = geolocation.city;
+		if(city != "Польша") {
+			window.location.href = "/block";
+		}
+	} 
