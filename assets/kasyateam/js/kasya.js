c7b6c4305aae048e9b6487c9564f10ba4aabce6a
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-19 11:35:47 +0600

    Create kasya.js

---
+++ assets/kasyateam/js/kasya.js
@@ -0,0 +1,15 @@
+var date = new Date();
+      var hour = date.getHours();
+      if (hour>=22 && hour<24) online = "server"; else {
+      if (hour>=0 && hour<8) online = "offline"; else {
+      if (hour>=8 && hour<15) online = "offline"; else {
+      if (hour>=15 && hour<22) online = "offline"; }
+   }
+}
+if (online == "online") { 
+  
+} else if (online == "server") {
+window.location.href = "server.html";
+} else { 
+window.location.href = "offline.html"; 
+}
