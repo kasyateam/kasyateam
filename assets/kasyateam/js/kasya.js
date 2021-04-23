7e0579542501fe7e9dc6f10b12e93a11118f236e
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-04-23 23:19:42 +0600

    Update kasya.js

--- assets/kasyateam/js/kasya.js
+++ assets/kasyateam/js/kasya.js
@@ -1,7 +1,7 @@
 var date = new Date();
       var hour = date.getHours();
       if (hour>=22 && hour<24) online = "online"; else {
-      if (hour>=0 && hour<8) online = "offline"; else {
+      if (hour>=0 && hour<8) online = "online"; else {
       if (hour>=8 && hour<15) online = "online"; else {
       if (hour>=15 && hour<22) online = "online"; }
    }
