b91ba3b2f5e1ea20823b714a3d41a7804e8fe6c2
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-05-15 17:57:03 +0600

    Update and rename test.js to assets/test.js

---
+++ assets/test.js
@@ -0,0 +1,49 @@
+document.writeln("
+
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://api-maps.yandex.ru/2.0/?load=package.standard&lang=ru-RU" type="text/javascript"></script> 
+  <script src="assets/kasyateam/js/VPN.js"></script> 
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/kasya.png-122x118.png" type="image/x-icon">
+  <meta name="description" content="Искомой страницы не существует">
+  <title>404</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/animatecss/animate.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/kasyateam/css/additional.css" type="text/css">
+  <script src="assets/kasyateam/js/kasya.js"></script>   
+</head>
+<body>
+<div id="get_info" style="display:none;"></div>
+  <section class="header5 cid-suTJsbxx7d fullscreen" id="header5-s">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="white col-md-10">
+                <h1 class="section-title align-center pb-3 fonts-style display-1"><strong>404</strong></h1>
+                <p class="text align-center display-5 pb-3 fonts-style">Искомой страницы не существует или она была перемещена</p>
+                <div class="section-btn align-center"><a class="btn btn-md btn-white-outline display-4" href="/main">На главную</a></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/viewportchecker/jquery.viewportchecker.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+  <input name="animation" type="hidden">
+  </body>
+</html>
+
+");
