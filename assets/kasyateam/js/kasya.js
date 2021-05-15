aeaa85386cb622ec34cf91b669db0e37314a86ba
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-05-15 17:50:32 +0600

    Update kasya.js

--- assets/kasyateam/js/kasya.js
+++ assets/kasyateam/js/kasya.js
@@ -1,7 +1,3 @@
-header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1.
-header('Pragma: no-cache'); // HTTP 1.0.
-header('Expires: 0'); // Proxies.
-
 var date = new Date();
       var hour = date.getHours();
       if (hour>=10 && hour<24) online = "online"; else {
