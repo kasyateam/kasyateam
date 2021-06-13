759d8f41228490903495aa5601b7fc2b4c951ec6
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-06-13 19:54:03 +0600

    Add files via upload

---
+++ why/whycheck.js
@@ -0,0 +1,4 @@
+function check () {
+var img = document.getElementById('view');
+img.innerHTML = '<img id="check" style="width: 100%; height: 100%;" src="http://mini.s-shot.ru/1280x1080/8000/png/?'+document.getElementById("check").value+'"></div>'
+}
