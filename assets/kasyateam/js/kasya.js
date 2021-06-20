0dc6cc162a4241ae1113057a416fc15990ece21c
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-06-21 02:39:38 +0600

    Update kasya.js

--- assets/kasyateam/js/kasya.js
+++ assets/kasyateam/js/kasya.js
@@ -1,15 +1,16 @@
 var date = new Date(); 
       var hour = date.getUTCHours();
-      if (hour>=18 && hour<19) online = "online";
-      else { if (hour>=19 && hour<24) online = "online";
-      else { if (hour>=0 && hour<1) online = "online";
-      else {  if (hour>=1&& hour<18) online = "online"; 
+      if (hour>=18 && hour<19) online = "offline";
+      else { if (hour>=19 && hour<24) online = "offline";
+      else { if (hour>=0 && hour<1) online = "server";
+      else {  if (hour>=1&& hour<18) online = "offline"; 
       }
    }
 }
 if (online == "online") {
 }else if (window.location.pathname == '/offline' && online == 'offline'){
 }else if (window.location.pathname == '/server' && online == 'server'){
+}else if (window.location.pathname == '/server-tick' && online == 'server'){
 }else if (online == "offline") {
 window.location.href = "/offline";
 }else if (online == "server") {
