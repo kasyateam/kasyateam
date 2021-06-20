c8dcd37f23774b6da9d56774ae959c61f33e810c
KASYA Team <70856891+kasyateam@users.noreply.github.com>
2021-06-20 14:41:16 +0600

    Update VPN.js

--- assets/kasyateam/js/VPN.js
+++ assets/kasyateam/js/VPN.js
@@ -1,11 +1,12 @@
 ymaps.ready(init);
-function init() {
+	function init() {
 		var geolocation = ymaps.geolocation, 
 		coords = [geolocation.latitude, geolocation.longitude], 
 		myMap = new ymaps.Map("get_info", {center: coords, zoom: 10});
 		var country = geolocation.country;
 		var city = geolocation.city;
-		if (country == " ") {
+		if (country == "undefined") {
+                window.location.href = "/error";
                 } else if (country == "Росfсия") { 
                 } else if (country == "Польша") {   
                 } else if (country == "Беларусь") {
