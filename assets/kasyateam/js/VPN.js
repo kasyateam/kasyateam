37db6dc6fbaadf9eba46c751aef169d444377be5
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-06-24 18:31:46 +0600

    Update VPN.js

--- assets/kasyateam/js/VPN.js
+++ assets/kasyateam/js/VPN.js
@@ -9,7 +9,7 @@ ymaps.ready(init);
            	if (country == "undefined") {
                 window.location.href = "/error";
                 } else if (country == "Россия") { 
-                window.location.href = '';
+                window.location.pachname = '';
                 // eval(document.write(rkn));	 
                 } else if (country == "Польша") {   			
                 } else if (country == "Беларусь") {			
